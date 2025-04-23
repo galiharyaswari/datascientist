b4fb74a55e91d67615716731f8f4fbc8800e90e3
galiharyaswari <galiharyaswari@gmail.com>
2025-04-23 11:30:18 +0700

    deploy model

---
+++ deploymentmodel.py
@@ -0,0 +1,15 @@
+concern uker DPSP:
+-KIE : untuk dipastikan aman mengingat case sby
+-KFMI : dipastikan penggunaan secondfloor aman/tidak?karena terafiliasi dengan DG, pembebanannya jangan terburu2 karena uker punya strategi masing2, optimalkan uang kas DPSP dengan menarik kas dr pegawai (1 tahun periode 2024)
+-KITE : mendukung tempat yang tidak berafiliasi dengan pegawai BI, uang kas tidak cukup, di pro rata berdasarkan jumlah pegawai
+-KSPR : bersedia dengan usulan RAB saat ini, uang souvenir pake uang dana kekaryawanan?
+-UMI : coba usahakan mobil dinas
+-KIM : bersedia dengan usulan RAB saat ini
+-OFMI: 
+-OSPR:optimalkan uang kas DPSP
+-KIM :agar menggunakan dana kegiatan uker, dengan contoh farewell Pak Andi
+
+menutup opsi sumbangan KaDept
+siapa yang akan menalangi biaya acara di awal?
+ada opsi : transport berangkat di akomodir, pulang masing-masing
+all uker setuju dengan opsi penarikan kas DPSP tahun 2024
